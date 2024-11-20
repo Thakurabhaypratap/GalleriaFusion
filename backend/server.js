3535139708fe0fd866e22952f39dc4e83ad080fa
Abhay <zzxxcc6386@gmail.com>
2024-11-21 00:26:18 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,37 @@
 import express from 'express';
-
+import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Node.js Docker App!' });
+  res.send('Hello, World!');
+});
+
+app.get('/api/jokes', (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      joke: 'joke1',
+    },
+    {
+      id: 2,
+      joke: 'joke2',
+    },
+    {
+      id: 3,
+      joke: 'joke3',
+    },
+    {
+      id: 4,
+      joke: 'joke4',
+    },
+    // Add more jokes here...
+  ];
+
+  res.json(jokes); // Use res.json to send the array as a JSON response
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
--- frontend/src/tests/App.test.js
+++
@@ -1,4 +0,0 @@
-test('renders correctly', () => {
-    expect(true).toBe(true);
-  });
-  
